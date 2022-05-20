4d9643a6bd83c0964e2780c958c547c3f7f9d939
LunaTheFoxgirl <cli-luna@protonmail.com>
2022-05-20 15:53:23 +0200

    add package.d

---
+++ inmath/package.d
@@ -0,0 +1,14 @@
+/**
+    inmath
+
+    Imports the most important parts of inmath
+
+    Authors: Inochi2D Project
+    License: MIT
+*/
+module inmath;
+
+public import inmath.linalg;
+public import inmath.math;
+public import inmath.util;
+public import inmath.interpolate;
